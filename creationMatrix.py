d0c9245edc5d57a77450ff9a073fa149e4584241
HaroldGal <harold.gallice@gmail.com>
2018-12-14 12:42:32 +0100

    debut crea matrice

---
+++ creationMatrix.py
@@ -0,0 +1,12 @@
+from scipy import sparse 
+
+def creationA(Triangles, Points)
+	A = sparse.lil_matrix((range(Points), range(Points))) 
+	for K in Triangles:
+		for i in range(K):
+			for j in range(K):
+				A[i,j] += a_{p}(phi_J, phi_I) // forme a(.,.) restreinte au triangle K_p
+
+		  B[i] += l_{p}(phi_I)$ // forme l(.) restreinte au triangle K_p
+
+
