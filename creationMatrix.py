b1db479196e9d830c2cab5d56e1b206171916ae8
KayimSaidB <kayimsaid@hotmail.fr>
2019-01-26 15:27:18 +0100

    k et alpha en argument

--- creationMatrix.py
+++ creationMatrix.py
@@ -4,15 +4,13 @@ from numpy import append as np_app
 from math import cos,sqrt,sin
 from cmath import exp
 import numpy as np
-alpha=np.pi
-
-
-def uinc(x,y):
-	k=10
+#alpha=np.pi/2
+#k=50
+def uinc(x,y,k,alpha):
+	
 	return  exp(np.complex(0,1)*k*(x*cos(alpha)+y*sin(alpha)))
 	#return sin(k*x/2.0)*sin(k*y/2.0)
 class Solver:
-	k = 10
 	def __init__(self, _triangles, _points, _bord_in,_bord_out):
 		self.triangles = _triangles
 		self.points = _points
@@ -34,13 +32,13 @@ class Solver:
 					data.append(aireK/12. * (2 if i==j else 1))
 		self.M = coo_matrix((array(data), (array(row_ind), array(col_ind))), shape=(len(self.points),len(self.points))).tocsr()
 
-	def creationMatriceMasseHelmholtz(self):
+	def creationMatriceMasseHelmholtz(self,k,alpha):
 		row_ind = []
 		col_ind = []
 		data = []
-		k=10 #nombre onde
+		 #nombre onde
 		for K in self.triangles:
-
+			
 			(x1,x2,x3) = (self.points[K[0]], self.points[K[1]], self.points[K[2]])
 			aireK = abs(((x2[0]-x1[0])*(x3[1]-x1[1]) - (x3[0]-x1[0])*(x2[1]-x1[1]))/2.)
 			for i in range(len(K)):
@@ -49,14 +47,14 @@ class Solver:
 					col_ind.append(K[j])
 					mydata=(np.complex(1,0)*k*k*aireK/12. * (2 if i==j else 1))
 					data.append(mydata)
-
+		
 		self.M = coo_matrix((array(data), (array(row_ind), array(col_ind))), shape=(len(self.points),len(self.points))).tocsr()
-
-	def creationMatriceMasseBordHelmholtz(self):
+	
+	def creationMatriceMasseBordHelmholtz(self,k,alpha):
 		row_ind = []
 		col_ind = []
 		data = []
-		k=10 #nombre onde
+		#k=50 #nombre onde
 		for K in self.bord_out:
 			(x1,x2) = (self.points[K[0]], self.points[K[1]])
 			sigma = np.sqrt((x2[0]-x1[0])**2 + (x2[1]-x1[1])**2)
@@ -75,7 +73,7 @@ class Solver:
 		col_ind = []
 		data = []
 		for K in self.triangles:
-
+			
 			(x1,x2,x3) = (self.points[K[0]], self.points[K[1]], self.points[K[2]])
 			aireK = abs(((x2[0]-x1[0])*(x3[1]-x1[1]) - (x3[0]-x1[0])*(x2[1]-x1[1]))/2.)
 			B = 1./(2.*aireK) * np.matrix([[x3[1]-x1[1], x1[1]-x2[1]], [x1[0]-x3[0],x2[0]-x1[0]]])
@@ -86,8 +84,8 @@ class Solver:
 					col_ind.append(K[j])
 					mydata=(-aireK * (phi[j].getT() * transform * phi[i])).item(0)
 					data.append(mydata)
-
-
+					
+			
 		self.D = coo_matrix((array(data), (array(row_ind), array(col_ind))), shape=(len(self.points),len(self.points))).tocsr()
 
 
@@ -103,14 +101,14 @@ class Solver:
 			for i in A:
 				self.b[i] = quad
 
-	def creationVecteurBHelmholtz(self):
+	def creationVecteurBHelmholtz(self,k,alpha):
 
 		self.b = np.zeros(len(self.points),np.complex128)
 
 		for aret in self.bord_in:
 			(x1,x2) = (self.points[aret[0]], self.points[aret[1]])
-			value1=-uinc(x1[0],x1[1])
-			value2=-uinc(x2[0],x2[1])
+			value1=-uinc(x1[0],x1[1],k,alpha)
+			value2=-uinc(x2[0],x2[1],k,alpha)
 			self.b[aret[0]]=value1
 			self.b[aret[1]]=value2
 
@@ -124,16 +122,17 @@ class Solver:
 			#Dtemp[:,btest[1]]=0
 			Dtemp[btest[1],btest[1]]=1
 		#data=Dtemp.flatten()
-		#print(data.shape)
+		#(data.shape)
 		self.A =csr_matrix(Dtemp)
 
 	def solve(self):
 		self.U  = np.linalg.solve((self.M+self.D).toarray(), self.b)
 
-	def newsolve(self):
+	def newsolve(self,k,alpha):
 		#on recupere la solution
 		self.U  = np.linalg.solve((self.A).toarray(), self.b)
 		#on recree le champ physique en parcourant tous les points
 		for i  in range(len(self.points)):
 			(x1,x2,x3)=self.points[i]
-			self.U[i]=abs(self.U[i]+uinc(x1,x2))
+			self.U[i]=abs(self.U[i]+uinc(x1,x2,k,alpha))
+
--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from LectureFichier import lectureFichier
 from creationMatrix import Solver
 from creationFileParaview import createVisu
 import numpy as np
+import sys
 liste_position,liste_triangle,bords_in,bords_out=lectureFichier("maillage/easySurf.msh")
 Elem = Solver(liste_triangle,liste_position,bords_in,bords_out)
-print(bords_out)
 #a = Solver([[2,1,0]],[[0.,0.],[1.0,0.],[0.,1.0]],bords,bords)
 #Elem.creationMatriceMass()
-Elem.creationMatriceMasseHelmholtz()
-Elem.creationMatriceMasseBordHelmholtz()
+alpha=np.pi
+k=10
+if (len(sys.argv)==3):
+	k=float(sys.argv[1])
+	alpha=float(sys.argv[1])
+
+Elem.creationMatriceMasseHelmholtz(k,alpha)
+Elem.creationMatriceMasseBordHelmholtz(k,alpha)
 Elem.creationMatriceRigidite()
 #Elem.creationVecteurB()
-Elem.creationVecteurBHelmholtz()
+Elem.creationVecteurBHelmholtz(k,alpha)
 Elem.creationMatriceAHelmholtz()
-Elem.newsolve()
+Elem.newsolve(k,alpha)
 createVisu(Elem.U, liste_position,liste_triangle)
