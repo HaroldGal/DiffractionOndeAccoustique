9b6eb2824d7a2cc629e7fc271e8c7fbe2acb0de0
KayimSaidB <kayimsaid@hotmail.fr>
2019-01-28 22:21:30 +0100

    late registration

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from creationMatrix import Solver
 from creationFileParaview import createVisu
 import numpy as np
 import sys
+import time
 liste_position,liste_triangle,bords_in,bords_out=lectureFichier("maillage/easySurf.msh")
 Elem = Solver(liste_triangle,liste_position,bords_in,bords_out)
 #a = Solver([[2,1,0]],[[0.,0.],[1.0,0.],[0.,1.0]],bords,bords)
@@ -12,7 +13,7 @@ Elem = Solver(liste_triangle,liste_position,bords_in,bords_out)
 #3 argument 1 si on veut générer pour un seul alpha ou un seul k auquel cas il faut choisir alpha et k, 0 si on veut 0 si on veut plusieurs alpha pour k fixé
 
 alpha=np.pi
-k=3
+k=0.8
 if (len(sys.argv)==4):
 	k=float(sys.argv[2])
 	alpha=float(sys.argv[3])
@@ -42,11 +43,39 @@ elif(int(sys.argv[1])==1):
 
 
 else:
+	tempsbase=time.time()
+
 	Elem.creationMatriceMasseHelmholtz(k,alpha)
+	tempsMasseHelmholtz=time.time()-tempsbase
+	
+	tempsbase=time.time()
+	
 	Elem.creationMatriceMasseBordHelmholtz(k,alpha)
+	tempsMasseBordHelmholtz=time.time()-tempsbase
+	tempsbase=time.time()
+
 	Elem.creationMatriceRigidite()
-	#Elem.creationVecteurB()
+	tempsRigidite=time.time()-tempsbase
+	tempsbase=time.time()
+
 	Elem.creationVecteurBHelmholtz(k,alpha)
+	tempsvecteurB=time.time()-tempsbase
+	tempsbase=time.time()
+
 	Elem.creationMatriceAHelmholtz()
+	tempsMatriceA=time.time()-tempsbase
+	tempsbase=time.time()
+
 	Elem.newsolve(k,alpha)
-	createVisu(Elem.U, liste_position,liste_triangle,alpha)
+	tempssolve=time.time()-tempsbase
+	tempsbase=time.time()
+
+	createVisu(Elem.U, liste_position,liste_triangle,k,alpha)
+	tempsVisu=time.time()-tempsbase
+
+	print("tempsMasseHelmholtz =",tempsMasseHelmholtz)
+	print("tempsMasseBordHelmholtz =",tempsMasseBordHelmholtz)
+	print("tempsMatriceA =",tempsMatriceA)
+	print("tempsVecteurB =",tempsvecteurB)
+	print("tempsSolve =",tempssolve)
+	print("tempsVisu =",tempsVisu)
